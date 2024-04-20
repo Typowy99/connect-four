466c10c25f71b9563846a21320d8486b1bfadb63
Typowy99 <konradkunka99@gmail.com>
2024-04-20 16:14:38 +0200

    Added ready console game

--- script.js
+++ script.js
@@ -16,10 +16,50 @@ function gameboard() {
         }
     }
 
+    const checkGameEnd = () => {
+
+        const boardValues = board.map(row => row.map(cell => cell.getValue()));
+
+        // horizontal
+        for (let y = 0; y < row; y++) {
+            for (let x = 0; x <= (column - 4); x++) {
+                if(boardValues[y].slice(x, x + 4).every(item => item === boardValues[y][x] && item !== 0)) {
+                    return true;
+                }
+            }
+        }
+
+        //bias
+        for (let y = 3; y < row; y++) {
+            for (let x = 0; x <= (column - 4); x++) {
+                const group = [];
+                for (let i = 0; i < 4; i++) {
+                    group.push(boardValues[y - i][x + i]);
+                }
+                if (group.every(item => item === group[0] && item !== 0)) {
+                    return true;
+                }
+            }
+        }
+
+        const boardValuesColumn = boardValues[0].map((_, x) => boardValues.map(row => row[x]));
+
+        // vertical
+        for (let y = 0; y < boardValuesColumn.length; y++) {
+            for (let x = 0; x <= (boardValuesColumn[0].length - 4); x++) {
+                if(boardValuesColumn[y].slice(x, x + 4).every(item => item === boardValuesColumn[y][x] && item !== 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
     const getBoard = () => board;
 
     // Add token to board, if column is full function will return false
-    const addTokenToBoard = (column, player) => {
+    const addTokenToBoard = (column, playerToken) => {
         const availableCells = board
             .map(row => row[column])
             .filter(cell => cell.getValue() === 0);
@@ -31,13 +71,14 @@ function gameboard() {
 
         let lastAvailableCells = availableCells.length - 1;
 
-        board[lastAvailableCells][column].addToken(player);
+        board[lastAvailableCells][column].addToken(playerToken);
 
     }
 
     return {
         addTokenToBoard,
         getBoard,
+        checkGameEnd,
     }
 
 }
@@ -73,7 +114,14 @@ function gameController() {
 
     const playRound = (column) => {
         board.addTokenToBoard(column, getActivePlayer().getToken());
+
+        if(board.checkGameEnd()) {
+            return true
+        }
+
         switchActivePlayer();
+
+        return false;
     }
 
     return {
@@ -86,6 +134,9 @@ function gameController() {
 function ScreenController() {
     const boardElement = document.querySelector('.board');
     const turnElement = document.querySelector('.turn');
+    const infoElement = document.querySelector('.info');
+    const infoText = document.querySelector('.info-text');
+
     const game = gameController();
 
     const updateScreen = () => {
@@ -111,7 +162,10 @@ function ScreenController() {
         const selectedColumn = e.target.dataset.column;
         if (!selectedColumn) return;
 
-        game.playRound(selectedColumn);
+        if(game.playRound(selectedColumn)) {
+            infoElement.style.display = 'block';
+            infoText.textContent = `${game.getActivePlayer().getName()} won!`;
+        }
         updateScreen();
     }
     boardElement.addEventListener("click", clickHandlerBoard);
